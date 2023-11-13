9325c829aee964aba0e3fc96e2f291a6158bcf3a
VincentDoherty <117232602+VincentDoherty@users.noreply.github.com>
2023-11-13 20:47:34 +0000

    Initial changes to BetterBasket

--- catalogue/BetterBasket.java
+++ catalogue/BetterBasket.java
@@ -15,4 +15,10 @@ public class BetterBasket extends Basket implements Serializable
   
 
   // You need to add code here
+  // let's start by overriding the add method
+  @Override
+  public boolean add( Product pr )
+  {
+	 return super.add( pr ); 		// Call add in ArrayList 
+  }
 }
